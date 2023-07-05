7fc779504c67b1e2367abcbe241e33bf341a5fc3
Houman Masnavi <masnavi@outlook.com>
2023-07-05 07:00:17 +0330

    طراحی داشبورد ویوئر و داشبورد دیزاینر

---
+++ src/Chamran.Deed.Web.Host/Controllers/Stimulsoft/ConversionFunctions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Chamran.Deed.Web.Controllers.Stimulsoft
+{
+    public class ConversionFunctions
+    {
+        public static string ConvertToPersianCalendar(DateTime georgianDate)
+        {
+            // Create an instance of the PersianCalendar class
+            var persianCalendar = new PersianCalendar();
+
+            // Calculate the Persian year, month, and day
+            var pYear = persianCalendar.GetYear(georgianDate);
+            var pMonth = persianCalendar.GetMonth(georgianDate);
+            var pDay = persianCalendar.GetDayOfMonth(georgianDate);
+
+            // Format the Persian date as a string in the format "yyyy/MM/dd"
+            var persianDate = $"{pYear:0000}/{pMonth:00}/{pDay:00}";
+
+            return persianDate;
+        }
+    }
+}
