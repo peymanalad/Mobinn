d02102a617a004a21cd3a51f49fdfaacfc3e2b0d
Houman Masnavi <masnavi@outlook.com>
2023-07-05 07:00:17 +0330

    طراحی داشبورد ویوئر و داشبورد دیزاینر

---
+++ src/Chamran.Deed.Web.Host/Controllers/Stimulsoft/DashboardController.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using Abp.AspNetCore.Mvc.Authorization;
+using Abp.Auditing;
+using Abp.Configuration.Startup;
+using System.Threading.Tasks;
+using Abp.Auditing;
+using Abp.Authorization;
+using Abp.Authorization.Users;
+using Abp.Configuration.Startup;
+using Abp.UI;
+using Microsoft.AspNetCore.Mvc;
+using Chamran.Deed.Authorization;
+using Chamran.Deed.Authorization.Accounts;
+using Chamran.Deed.Authorization.Accounts.Dto;
+using Chamran.Deed.Authorization.Users;
+using Chamran.Deed.Identity;
+using Chamran.Deed.MultiTenancy;
+using Chamran.Deed.Web.Models.Ui;
+using Chamran.Deed.Web.Session;
+using Stimulsoft.Report.Mvc;
+using Abp.Domain.Repositories;
+using Chamran.Deed.Info;
+using Chamran.Deed.People;
+using Abp.Domain.Uow;
+using Stimulsoft.Report.Dictionary;
+using Stimulsoft.Report.Web;
+using Stimulsoft.Base;
+using System.IO;
+
+namespace Chamran.Deed.Web.Controllers.Stimulsoft
+{
+    [AbpMvcAuthorize(AppPermissions.Pages_Administration)]
+    public class DashboardController : DeedControllerBase
+    {
+        private readonly IPerRequestSessionCache _sessionCache;
+        private readonly IMultiTenancyConfig _multiTenancyConfig;
+        private readonly IAccountAppService _accountAppService;
+        private readonly LogInManager _logInManager;
+        private readonly SignInManager _signInManager;
+        private readonly AbpLoginResultTypeHelper _abpLoginResultTypeHelper;
+        private readonly IRepository<Report> _reportRepository;
+        private readonly IRepository<OrganizationGroup> _organizationGroupRepository;
+        private readonly IRepository<GroupMember> _groupMemberRepository;
+        private readonly IUnitOfWorkManager _unitOfWorkManager;
+
+        public DashboardController(
+            IPerRequestSessionCache sessionCache,
+            IMultiTenancyConfig multiTenancyConfig,
+            IAccountAppService accountAppService,
+            LogInManager logInManager,
+            SignInManager signInManager,
+            AbpLoginResultTypeHelper abpLoginResultTypeHelper,
+            IRepository<Report> reportRepository,
+            IRepository<OrganizationGroup> organizationGroupRepository,
+            IRepository<GroupMember> groupMemberRepository,
+            IUnitOfWorkManager unitOfWorkManager)
+        {
+            _sessionCache = sessionCache;
+            _multiTenancyConfig = multiTenancyConfig;
+            _accountAppService = accountAppService;
+            _logInManager = logInManager;
+            _signInManager = signInManager;
+            _abpLoginResultTypeHelper = abpLoginResultTypeHelper;
+            _reportRepository = reportRepository;
+            _organizationGroupRepository = organizationGroupRepository;
+            _groupMemberRepository = groupMemberRepository;
+            _unitOfWorkManager = unitOfWorkManager;
+        }
+        // GET
+        [DisableAuditing]
+        public async Task<IActionResult> Index()
+        {
+            var model = new HomePageModel
+            {
+                LoginInformation = await _sessionCache.GetCurrentLoginInformationsAsync(),
+                IsMultiTenancyEnabled = _multiTenancyConfig.IsEnabled
+            };
+
+            //if (model.LoginInformation?.User == null)
+            //{
+            //    return RedirectToAction("Login");
+            //}
+
+            return View(model);
+        }
+
+        public Task<IActionResult> Design()
+        {
+            // Check if the user is logged in or redirect to the login page if not
+
+            var options = new StiNetCoreDesignerOptions
+            {
+                Actions =
+                {
+                    GetReport = "GetDesignReport",
+                    DesignerEvent = "DesignerEvent",
+                    SaveReport = "SaveReport",
+                },
+                Behavior =
+                {
+                    //SaveReportAsMode = StiSaveMode.Hidden
+                    ShowSaveDialog = false,
+
+                },
+                Appearance =
+                {
+                    ShowDialogsHelp = false,
+                    ShowSystemFonts = false,
+
+                },
+                FileMenu =
+                {
+                    ShowAbout = false,
+                    ShowExit = false,
+                    ShowClose = false,
+                    ShowFileMenuNewDashboard = true,
+                    ShowFileMenuNewReport = false,
+                    ShowHelp = false,
+                    ShowInfo = false,
+
+                },
+                Dictionary =
+                {
+                    //PermissionSqlParameters = StiDesignerPermissions.View,
+                    PermissionDataSources = StiDesignerPermissions.View,
+                    PermissionDataConnections = StiDesignerPermissions.View,
+
+                }
+            };
+
+            //// Loading and adding a font to resources
+            //var fontPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot","Fonts", "IRANSans.ttf");
+            //var fontContent = System.IO.File.ReadAllBytes(fontPath);
+            //var resource = new StiResource("IRANSans", "IRANSans", false, StiResourceType.FontTtf, fontContent, false);
+            //StiFontCollection.AddResourceFont(resource.Name, resource.Content, "ttf", resource.Alias);
+
+
+
+            // Add the custom function
+            StiFunctions.AddFunction(
+                "Conversion",
+                "ConvertToPersianCalendar",
+                "ConvertToPersianCalendar",
+                "Converts a Georgian date to a Persian date By DrMas",
+                typeof(ConversionFunctions),
+                typeof(string),
+                "Returns the converted Persian date",
+                new[] { typeof(DateTime) },
+                new[] { "georgianDate" },
+                new[] { "The Georgian date to convert" }
+            );
+
+            ViewBag.Options = options;
+
+            return Task.FromResult<IActionResult>(View());
+        }
+
+        public async Task<IActionResult> GetReport()
+        {
+            //var appPath = StiNetCoreHelper.MapPath(this, string.Empty);
+            var loginInformation = await _sessionCache.GetCurrentLoginInformationsAsync();
+            var dashboard = await Helpers.DashboardHelper.GetCurrentOrganizationDashboard(_reportRepository, _organizationGroupRepository, _groupMemberRepository, loginInformation.User.Id);
+
+            //ToDo:MapData to Dashboard
+
+            return StiNetCoreViewer.GetReportResult(this, dashboard);
+        }
+
+        public async Task<IActionResult> GetDesignReport()
+        {
+            //var appPath = StiNetCoreHelper.MapPath(this, string.Empty);
+            var loginInformation = await _sessionCache.GetCurrentLoginInformationsAsync();
+            var dashboard = await Helpers.DashboardHelper.GetCurrentOrganizationDashboard(_reportRepository, _organizationGroupRepository, _groupMemberRepository, loginInformation.User.Id);
+
+            //ToDo:MapData to Dashboard
+
+            // Loading and adding a font to resources
+            var fontPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Fonts", "IRANSans.ttf");
+            var fontContent = await System.IO.File.ReadAllBytesAsync(fontPath);
+            var resource = new StiResource("IRANSans", "IRANSans", false, StiResourceType.FontTtf, fontContent, false);
+            if (!dashboard.Dictionary.Resources.Contains("IRANSans"))
+                dashboard.Dictionary.Resources.Add(resource);
+
+            fontPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Fonts", "IRANSans(FaNum)_Medium.ttf");
+            fontContent = await System.IO.File.ReadAllBytesAsync(fontPath);
+            resource = new StiResource("IRANSans(FaNum) Medium", "IRANSans(FaNum)", false, StiResourceType.FontTtf, fontContent, false);
+            if (!dashboard.Dictionary.Resources.Contains("IRANSans(FaNum) Medium"))
+                dashboard.Dictionary.Resources.Add(resource);
+
+
+
+
+            return StiNetCoreDesigner.GetReportResult(this, dashboard);
+        }
+
+        public IActionResult ViewerEvent()
+        {
+            return StiNetCoreViewer.ViewerEventResult(this);
+        }
+
+        #region Designer Events
+        public IActionResult DesignerEvent()
+        {
+            return StiNetCoreDesigner.DesignerEventResult(this);
+        }
+
+        public IActionResult OnGetDesignerEvent()
+        {
+            return StiNetCoreDesigner.DesignerEventResult(this);
+        }
+
+        public IActionResult OnPostDesignerEvent()
+        {
+            return StiNetCoreDesigner.DesignerEventResult(this);
+        }
+
+        #endregion
+
+        public async Task<IActionResult> SaveReport()
+        {
+            var report = StiNetCoreDesigner.GetReportObject(this);
+            var loginInformation = await _sessionCache.GetCurrentLoginInformationsAsync();
+
+            using (var unitOfWork = _unitOfWorkManager.Begin())
+            {
+                await Helpers.DashboardHelper.SaveCurrentOrganizationDashboard(report, _reportRepository, _organizationGroupRepository, _groupMemberRepository, loginInformation.User.Id);
+
+                await unitOfWork.CompleteAsync();
+            }
+
+            return await StiNetCoreDesigner.SaveReportResultAsync(this);
+        }
+    }
+}
---
+++ src/Chamran.Deed.Web.Host/Helpers/DashboardHelper.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using Abp.Domain.Repositories;
+using Abp.Domain.Uow;
+using Abp.Logging;
+using Abp.UI;
+using Chamran.Deed.Info;
+using Chamran.Deed.People;
+using Microsoft.EntityFrameworkCore;
+using Stimulsoft.Base.Drawing;
+using Stimulsoft.Dashboard.Components;
+using Stimulsoft.Dashboard.Components.Table;
+using Stimulsoft.Data.Engine;
+using Stimulsoft.Report;
+using Z.EntityFramework.Plus;
+
+namespace Chamran.Deed.Web.Helpers
+{
+    public class DashboardHelper
+    {
+        /*public static StiReport CreateTemplate(string appPath)
+        {
+            var report = StiReport.CreateNewDashboard();
+            var dashboard = report.Pages[0] as StiDashboard;
+
+            //var dataPath = Path.Combine(appPath, "Data/Demo.xml");
+            //var data = new DataSet();
+            //data.ReadXml(dataPath);
+
+            //report.RegData(data);
+            report.Dictionary.Synchronize();
+
+            var tableElement = new StiTableElement
+            {
+                Left = 0,
+                Top = 0,
+                Width = 700,
+                Height = 500,
+                Border =
+                {
+                    Side = StiBorderSides.All
+                },
+                BackColor = Color.LightGray,
+                Name = "Example"
+            };
+
+            var dataBase = new StiDimensionColumn
+            {
+                Expression = "Products.ProductID"
+            };
+            tableElement.Columns.Add(dataBase);
+
+            var dataBase1 = new StiDimensionColumn
+            {
+                Expression = "Products.ProductName"
+            };
+            tableElement.Columns.Add(dataBase1);
+
+            var dataBase2 = new StiDimensionColumn
+            {
+                Expression = "Products.UnitPrice"
+            };
+            tableElement.Columns.Add(dataBase2);
+
+            var filter1 = new StiDataFilterRule
+            {
+                Condition = StiDataFilterCondition.BeginningWith,
+                Path = "Products.ProductID",
+                Value = "1"
+            };
+            tableElement.DataFilters.Add(filter1);
+
+            var filter2 = new StiDataFilterRule
+            {
+                Condition = StiDataFilterCondition.EndingWith,
+                Path = "Products.UnitPrice",
+                Value = "1"
+            };
+            tableElement.DataFilters.Add(filter2);
+
+            dashboard?.Components.Add(tableElement);
+
+            return report;
+        }*/
+
+        public static async Task<StiReport> GetCurrentOrganizationDashboard(IRepository<Report> reportRepository,
+            IRepository<OrganizationGroup> organizationGroupRepository, IRepository<GroupMember> groupMemberRepository,
+            long userId)
+        {
+            var query = reportRepository.GetAll().Where(x => x.IsDashboard);
+            var query2 = from report in query
+                         join orgGroup in organizationGroupRepository.GetAll() on report.OrganizationId equals orgGroup
+                             .OrganizationId into joined1
+                         from orgGroup in joined1.DefaultIfEmpty()
+                         join grpMember in groupMemberRepository.GetAll() on orgGroup.OrganizationId equals grpMember
+                             .OrganizationGroupId into joined2
+                         from grpMember in joined2.DefaultIfEmpty()
+                         where grpMember.UserId == userId
+                         select new
+                         {
+                             report.ReportContent
+                         };
+            string reportContent = null;
+            if (query2.Any())
+            {
+                var result = await query2.FirstAsync();
+                reportContent = result.ReportContent;
+
+            }
+
+            if (!string.IsNullOrEmpty(reportContent)) return GetReportFromContent(reportContent);
+            var orgQuery =
+                from orgGroup in organizationGroupRepository.GetAll().Include(orgGroup => orgGroup.OrganizationFk)
+                join grpMember in groupMemberRepository.GetAll() on orgGroup.OrganizationId equals grpMember
+                    .OrganizationGroupId into joined2
+                from grpMember in joined2.DefaultIfEmpty()
+                where grpMember.UserId == userId
+                select orgGroup.OrganizationFk;
+
+            if (!orgQuery.Any())
+            {
+                throw new UserFriendlyException("کاربر عضو هیچ گروهی در هیچ سازمانی نمی باشد");
+            }
+            return CreateEmptyReport(userId, orgQuery.First(), reportRepository);
+
+
+        }
+
+        private static StiReport GetReportFromContent(string reportContent)
+        {
+            try
+            {
+                var report = StiReport.CreateNewDashboard();
+                report.LoadEncryptedReportFromString(reportContent, "DrM@s");
+                return report;
+            }
+            catch (Exception)
+            {
+                //ignored
+            }
+            return GetTemplateDashboard();
+            
+        }
+
+        private static StiReport CreateEmptyReport(long userId, Organization org, IRepository<Report> reportRepository)
+        {
+            var report = GetTemplateDashboard();
+            reportRepository.Insert(new Report()
+            {
+                OrganizationId = org.Id,
+                IsDashboard = true,
+                ReportContent = report.SaveEncryptedReportToString("DrM@s"),
+                CreationTime = DateTime.Now,
+                CreatorUserId = userId,
+                ReportDescription = "داشبورد سازمانی " + org.OrganizationName
+            });
+
+            return report;
+
+        }
+
+        private static StiReport GetTemplateDashboard()
+        {
+
+            var report = StiReport.CreateNewDashboard();
+            var dashboard = report.Pages[0] as StiDashboard;
+
+            //var dataPath = Path.Combine(appPath, "Data/Demo.xml");
+            //var data = new DataSet();
+            //data.ReadXml(dataPath);
+
+            //report.RegData(data);
+            report.Dictionary.Synchronize();
+
+            var tableElement = new StiTableElement
+            {
+                Left = 0,
+                Top = 0,
+                Width = 700,
+                Height = 500,
+                Border =
+                {
+                    Side = StiBorderSides.All
+                },
+                BackColor = Color.LightGray,
+                Name = "نمونه"
+            };
+
+            var dataBase = new StiDimensionColumn
+            {
+                Expression = "Products.ProductID"
+            };
+            tableElement.Columns.Add(dataBase);
+
+            var dataBase1 = new StiDimensionColumn
+            {
+                Expression = "Products.ProductName"
+            };
+            tableElement.Columns.Add(dataBase1);
+
+            var dataBase2 = new StiDimensionColumn
+            {
+                Expression = "Products.UnitPrice"
+            };
+            tableElement.Columns.Add(dataBase2);
+
+            var filter1 = new StiDataFilterRule
+            {
+                Condition = StiDataFilterCondition.BeginningWith,
+                Path = "Products.ProductID",
+                Value = "1"
+            };
+            tableElement.DataFilters.Add(filter1);
+
+            var filter2 = new StiDataFilterRule
+            {
+                Condition = StiDataFilterCondition.EndingWith,
+                Path = "Products.UnitPrice",
+                Value = "1"
+            };
+            tableElement.DataFilters.Add(filter2);
+
+            dashboard?.Components.Add(tableElement);
+
+            return report;
+        }
+
+        public static async Task SaveCurrentOrganizationDashboard(StiReport savedReport, IRepository<Report> reportRepository, IRepository<OrganizationGroup> organizationGroupRepository, IRepository<GroupMember> groupMemberRepository, long userId)
+        {
+                var query = reportRepository.GetAll().Where(x => x.IsDashboard);
+                var query2 = from report in query
+                    join orgGroup in organizationGroupRepository.GetAll() on report.OrganizationId equals orgGroup.OrganizationId into joined1
+                    from orgGroup in joined1.DefaultIfEmpty()
+                    join grpMember in groupMemberRepository.GetAll() on orgGroup.OrganizationId equals grpMember.OrganizationGroupId into joined2
+                    from grpMember in joined2.DefaultIfEmpty()
+                    where grpMember.UserId == userId
+                    select report;
+
+                if (query2.Any())
+                {
+                    var result = await query2.FirstAsync();
+                    result.ReportContent = savedReport.SaveEncryptedReportToString("DrM@s");
+                    result.LastModificationTime = DateTime.Now;
+                    result.LastModifierUserId = userId;
+
+                    await reportRepository.UpdateAsync(result);
+                }
+
+        }
+    }
+}
