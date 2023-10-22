9a74602ee4c78112902344ba8f281b73de719a2e
Houman Masnavi <masnavi@outlook.com>
2023-10-22 14:43:37 +0330

    اصلاح NoOrg

---
+++ src/Chamran.Deed.Application.Shared/People/Dtos/GetAllNoOrganizationDto.cs
@@ -0,0 +1,9 @@
+﻿using Abp.Application.Services.Dto;
+
+namespace Chamran.Deed.People.Dtos
+{
+    public class GetAllNoOrganizationDto: PagedAndSortedResultRequestDto
+    {
+        public int? OrganizationId { get; set; }
+    }
+}
