a88ff8ea3a69ddfca1ad8652977677c7977cf680
Houman Masnavi <info@drmasnavi.ir>
2023-11-19 05:01:12 +0330

    تغییرات 28 آبان ماه

---
+++ src/Chamran.Deed.Core/Info/GetEntriesDetail.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Chamran.Deed.Info;
+
+public class GetEntriesDetail
+{
+    public int Id { get; set; }
+    public string Caption { get; set; }
+    public Guid SharedTaskId { get; set; }
+    public int PostId { get; set; }
+    public long IssuerId { get; set; }
+    public long ReceiverId { get; set; }
+    public int? ParentId { get; set; }
+    public DateTime CreationTime { get; set; }
+    public long? CreatorUserId { get; set; }
+    public string IssuerFirstName { get; set; }
+    public string IssuerLastName { get; set; }
+    public Guid? IssuerProfilePicture { get; set; }
+    public string ReceiverFirstName { get; set; }
+    public string ReceiverLastName { get; set; }
+    public Guid? ReceiverProfilePicture { get; set; }
+
+    public bool ReturnedToParent { get; set; }
+    //public string IssuerMemberPos { get; set; }
+    //public string ReceiverMemberPos { get; set; }
+}
---
+++ src/Chamran.Deed.Core/Info/GetListOfUsers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Chamran.Deed.Info;
+
+public class GetListOfUsers
+{
+    public string NationalId { get; set; }
+    public string Name { get; set; }
+
+    public string Surname { get; set; }
+
+    public string UserName { get; set; }
+
+    public string EmailAddress { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+    public Guid? ProfilePictureId { get; set; }
+
+    //public List<UserListRoleDto> Roles { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public DateTime CreationTime { get; set; }
+    public DateTime? LastLoginAttemptTime { get; set; }
+}
