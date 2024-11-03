3052855b849548a70f251ffbdca649302ba761c5
Houman Masnavi <info@drmasnavi.ir>
2024-11-03 19:11:58 +0330

    تغییرات مدل

---
+++ src/Chamran.Deed.Application.Shared/Authorization/Accounts/Dto/AccountUserType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chamran.Deed.Authorization.Accounts.Dto
+{
+    public enum AccountUserType
+    {
+        Normal=1,
+        Distributer=2,
+        Creator=3,
+        Admin=4,
+        SuperAdmin=5
+    }
+}
