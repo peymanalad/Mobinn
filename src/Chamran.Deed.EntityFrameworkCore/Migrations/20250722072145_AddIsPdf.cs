710fd2172611b28a67da64855a2a9b6033384998
peyman <peyman.alad@gmail.com>
2025-07-22 11:36:37 +0330

    Add IsSlide and IsPdf

---
+++ src/Chamran.Deed.EntityFrameworkCore/Migrations/20250722072145_AddIsPdf.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Chamran.Deed.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsPdf : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "PdfFile",
+                table: "Posts",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_PdfFile",
+                table: "Posts",
+                column: "PdfFile");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_AppBinaryObjects_PdfFile",
+                table: "Posts",
+                column: "PdfFile",
+                principalTable: "AppBinaryObjects",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_AppBinaryObjects_PdfFile",
+                table: "Posts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Posts_PdfFile",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "PdfFile",
+                table: "Posts");
+        }
+    }
+}
