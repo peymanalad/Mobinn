ed9e9cb0697bd6c16b0ea56a204f7266742698b9
peyman <peyman.alad@gmail.com>
2025-07-23 14:21:02 +0330

    change kestrelServer True

--- src/Chamran.Deed.Core/Configuration/AppSettingProvider.cs
+++ src/Chamran.Deed.Core/Configuration/AppSettingProvider.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Abp.Configuration;
 using Abp.Extensions;
@@ -54,6 +55,28 @@ namespace Chamran.Deed.Configuration
                 .Union(GetTheme13Settings())
                 .Union(GetDashboardSettings())
                 .Union(GetExternalLoginProviderSettings());
+        }
+
+        public static string Get(string key, IConfiguration config)
+        {
+            var envValue = Environment.GetEnvironmentVariable(key);
+            if (!string.IsNullOrWhiteSpace(envValue))
+                return envValue;
+
+            var configKey = key.Replace("__", ":");
+            return config[configKey];
+        }
+
+        public static bool GetBool(string key, IConfiguration config, bool defaultValue = false)
+        {
+            var value = Get(key, config);
+            return bool.TryParse(value, out var result) ? result : defaultValue;
+        }
+
+        public static int GetInt(string key, IConfiguration config, int defaultValue = 0)
+        {
+            var value = Get(key, config);
+            return int.TryParse(value, out var result) ? result : defaultValue;
         }
 
         private void ChangeEmailSettingScopes(SettingDefinitionProviderContext context)
