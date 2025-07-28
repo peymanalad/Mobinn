9e0ccf29d6f62dbdd36f398db29a541ff7d97fe3
peyman <peyman.alad@gmail.com>
2025-07-28 14:17:59 +0330

    add remove bytes

--- src/Chamran.Deed.Core/Storage/DbBinaryObjectManager.cs
+++ src/Chamran.Deed.Core/Storage/DbBinaryObjectManager.cs
@@ -9,9 +9,11 @@ namespace Chamran.Deed.Storage
     public class DbBinaryObjectManager : IBinaryObjectManager, ITransientDependency
     {
         private readonly string _baseFolder;
+        private readonly IRepository<BinaryObject, Guid> _binaryObjectRepository;
 
-        public DbBinaryObjectManager()
+        public DbBinaryObjectManager(IRepository<BinaryObject, Guid> binaryObjectRepository)
         {
+            _binaryObjectRepository = binaryObjectRepository;
             _baseFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "BinaryObjects");
             if (!Directory.Exists(_baseFolder))
             {
@@ -46,6 +48,10 @@ namespace Chamran.Deed.Storage
 
             var filePath = GetFilePath(file.Id, file.Description);
             await File.WriteAllBytesAsync(filePath, file.Bytes);
+
+            //avoid storing large bytes in the database
+            file.Bytes = Array.Empty<byte>();
+            await _binaryObjectRepository.InsertAsync(file);
         }
 
         public async Task DeleteAsync(Guid id)
@@ -64,6 +70,7 @@ namespace Chamran.Deed.Storage
                     }
                 }
             }
+            await _binaryObjectRepository.DeleteAsync(id);
         }
 
         private string GetDirectoryPath(Guid id)
