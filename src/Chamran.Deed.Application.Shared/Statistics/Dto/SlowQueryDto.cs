3e5c55af76cf10fd8f990641389555d9e831e2f9
peyman <peyman.alad@gmail.com>
2025-08-16 18:02:19 +0330

    Add Analyze Services

---
+++ src/Chamran.Deed.Application.Shared/Statistics/Dto/SlowQueryDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Chamran.Deed.Statistics.Dto
+{
+    public class SlowQueryDto
+    {
+        public string ServiceName { get; set; }
+        public string MethodName { get; set; }
+        public int ExecutionDuration { get; set; }
+    }
+}
---
+++ src/Chamran.Deed.Application.Shared/Statistics/Dto/StatisticResultDto.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Chamran.Deed.Statistics.Dto
+{
+    /// <summary>
+    /// Generic container DTO for statistics endpoints.
+    /// </summary>
+    public class StatisticResultDto
+    {
+        public string Metric { get; set; }
+        public string Message { get; set; }
+        public IDictionary<string, object> Data { get; set; }
+    }
+}
