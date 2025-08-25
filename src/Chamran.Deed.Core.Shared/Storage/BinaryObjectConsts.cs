c9cf9c7886362aaeb33fef4372ffc1f24ddf7445
peyman <peyman.alad@gmail.com>
2025-08-25 11:12:44 +0330

    Change BytesMaxSize

--- src/Chamran.Deed.Core.Shared/Storage/BinaryObjectConsts.cs
+++ src/Chamran.Deed.Core.Shared/Storage/BinaryObjectConsts.cs
@@ -3,6 +3,6 @@
     public class BinaryObjectConsts
     {
         //public const int BytesMaxSize = 10240;
-        public const int BytesMaxSize = 10 * 1024 * 1024;
+        public const int BytesMaxSize = 20 * 1024 * 1024;
     }
 }
